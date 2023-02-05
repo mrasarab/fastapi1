79745a2a888d41f7b048a6b28e7d6c013e4b0a99
pooya <mras.arab@gmail.com>
2023-02-05 13:56:13 +0100

    first upload1

--- database.py
+++
@@ -1,9 +0,0 @@
-import gridfs
-from pymongo import MongoClient
-import json
-client = MongoClient(
-    "mongodb+srv://pooyaarab:zJMzoHr1fMhOnnHk@cluster0.vwrrya4.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
-fs = gridfs.GridFS(db)
-                
-
